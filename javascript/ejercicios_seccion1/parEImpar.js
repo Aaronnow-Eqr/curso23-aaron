abab6f80352e676ae315e6bdbd30c251335b4702
Aaronnow-Eqr <79377629+Aaronnow-Eqr@users.noreply.github.com>
2025-07-07 17:57:10 -0400

    .

--- javascript/ejercicios_seccion1/parEImpar.js
+++
@@ -1,9 +0,0 @@
-number = Number(prompt("Ingrese un número:"));
-
-if (number % 2 === 0) {
-  alert("El número " + number + " es par.");
-} else if (number % 2 === 1) {
-  alert("El número " + number + " es impar.");
-} else {
-  alert("El número ingresado no es un entero válido.");
-}
